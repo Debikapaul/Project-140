b78bfad97e177dd2e8c9886a6d8ed8c5e4580993
Debikapaul <82105390+Debikapaul@users.noreply.github.com>
2022-05-04 17:27:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,18 +21,16 @@ var ball = {
     dy:3
 }
 
-function setup() {
-	canvas = createCanvas(700,600);
-	canvas.parent('canvas');
-
-	video = createCapture(VIDEO);
-  video.size(800,400);
-	video.parent('game_console');
-
-	poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose', gotPoses);
-}
-
+function setup(){                                
+  var canvas = createCanvas(700,600);
+  canvas.parent('canvas');
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses); 
+}                        
+                  
 function draw(){
 
  background(0); 
